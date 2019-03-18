e7c5304933e183dea45cccfaad4daf0cd31fb8d6
Ala Dzemyanovich <ala.dzemyanovich@gmail.com>
2019-03-19 00:54:22 +0100

    feat: add function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  for (var i=0; i < preferences.length; i++) {
+    let firstSpichonee = preferences[i];
+    let secondSpichonee = preferences[firstSpichonee-1];
+    let thirdSpichonee = preferences[secondSpichonee-1];
+    if(firstSpichonee === secondSpichonee || firstSpichonee === thirdSpichonee || secondSpichonee === thirdSpichonee){
+      continue;
+    }
+    if (thirdSpichonee === i+1) {
+      count++;
+    }
+   }
+  return (count/3).toFixed(0);
 };
